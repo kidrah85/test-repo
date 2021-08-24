377a2710dd99cb6ad0c44a8251bd68653287b469
kidrah85 <kidrah85@gmail.com>
2021-08-23 20:42:52 -0700

    fries

--- hungry.py
+++ hungry.py
@@ -5,5 +5,6 @@ if hungry == 'yes':
     print('eat something healthy')
     print('eat something junk')
     print('eat something more junk')
+    print('eat fries')
 else:
     print('do your iv work and focus!')
