a680cf323a3355b8f53570cecf555212a5632226
kidrah85 <kidrah85@gmail.com>
2021-08-23 20:38:27 -0700

    water

--- hungry.py
+++ hungry.py
@@ -6,4 +6,6 @@ if hungry == 'yes':
     print('eat something junk')
     print('eat something more junk')
 else:
-    print('do your iv work and focus!')
+    thirsty = raw_input('are you thirsty?')
+    if thirsty == 'yes':
+        print('drink water')
