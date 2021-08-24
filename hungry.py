0c97466eb4168a1a33d46866ef8a25b8450e834a
kidrah85 <kidrah85@gmail.com>
2021-08-23 20:16:45 -0700

    added more junk

--- hungry.py
+++ hungry.py
@@ -4,5 +4,6 @@ hungry = raw_input('Are you hungry?')
 if hungry == 'yes':
     print('eat something healthy')
     print('eat something junk')
+    print('eat something more junk')
 else:
     print('do your iv work and focus!')
